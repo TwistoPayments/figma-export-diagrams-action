a100a56a6e3d0a1bb5131bccc303a13b73442460
paveldigana <pavel.digana@email.cz>
2023-01-12 19:10:37 +0100

    Implementation export of exportType

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,53 @@
 import * as core from '@actions/core'
-import { mkdirSync } from 'fs'
+import {mkdirSync} from 'fs'
 import path from 'path'
 
-import { run } from './runner'
+import {run} from './runner'
 
 const distFolder = 'dist'
 const [_bin, _sourcePath, outDir = path.resolve(__dirname, '..', distFolder)] = process.argv
 
 const jsonParse = <T>(text: string): T | false => {
-  try {
-    return JSON.parse(text)
-  } catch (error) {
-    if (error instanceof SyntaxError) {
-      return false
-    }
+    try {
+        return JSON.parse(text)
+    } catch (error) {
+        if (error instanceof SyntaxError) {
+            return false
+        }
 
-    throw error
-  }
+        throw error
+    }
 }
 
-const accessToken = core.getInput('accessToken', { required: true })
-const fileKey = core.getInput('fileKey', { required: true })
-const exportType = core.getInput('exportType', { required: true })
-const ids = jsonParse<string[]>(core.getInput('ids', { required: false }) || '[]')
+const accessToken = core.getInput('accessToken', {required: true})
+const fileKey = core.getInput('fileKey', {required: true})
+const exportType = core.getInput('exportType', {required: true}).toLowerCase()
+const ids = jsonParse<string[]>(core.getInput('ids', {required: false}) || '[]')
 
-;(async function() {
+;(async function () {
 
-  if (ids === false) {
-    core.setFailed('"ids" must be a stringified array of strings.')
-    return;
-  }
+    if (ids === false) {
+        core.setFailed('"ids" must be a stringified array of strings.')
+        return;
+    }
 
-  core.startGroup('Export diagrams')
-  const pdfs = await run({ accessToken, fileKey, ids, outDir , exportType})
-  console.log("End group");
-  core.endGroup()
+    // create exportType to enum 'jpg' | 'png' | 'svg' | 'pdf'
+    if (exportType !== 'jpg' && exportType !== 'png' && exportType !== 'svg' && exportType !== 'pdf') {
+        core.setFailed(`Unsupported export type: ${exportType}`)
+        return;
+    }
 
-  mkdirSync(path.resolve(outDir), { recursive: true })
+    core.startGroup('Export diagrams')
+    const pdfs = await run({accessToken, fileKey, ids, outDir, exportType})
+    console.log("End group");
+    core.endGroup()
 
-  if (pdfs.length === 0) {
-    core.warning('No Diagrams has been exported.')
-  }
+    mkdirSync(path.resolve(outDir), {recursive: true})
 
-  core.setOutput('pdfs', pdfs);
-  core.setOutput('outDir', `./${distFolder}/`)
+    if (pdfs.length === 0) {
+        core.warning('No Diagrams has been exported.')
+    }
+
+    core.setOutput('pdfs', pdfs);
+    core.setOutput('outDir', `./${distFolder}/`)
 })()
