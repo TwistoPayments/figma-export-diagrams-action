6163d72a16d18a0b9dae2a7061ece65f5d22b385
paveldigana <pavel.digana@email.cz>
2023-01-12 18:44:13 +0100

    Implementation export of exportType

--- src/export.ts
+++ src/export.ts
@@ -1,77 +1,87 @@
-import { Api, Node, NodeTypes } from 'figma-api'
-import { getGroups } from './figma'
+import {Api, Node, NodeTypes} from 'figma-api'
+import {getGroups} from './figma'
 
 export type FigmaExport = {
-  id: string
-  name: string
-  pages: string[],
-  cover: string
+    id: string
+    name: string
+    pages: string[],
+    cover: string
 }
 
 type Props = {
-  accessToken: string
-  fileKey: string
-  ids: string[]
+    accessToken: string
+    fileKey: string
+    ids: string[]
+    exportType: 'jpg' | 'png' | 'svg' | 'pdf'
 }
 
 const pagesAreOk = (pages: (string | null)[]): pages is string[] => {
-  return pages.find(page => typeof page !== 'string') === undefined
+    return pages.find(page => typeof page !== 'string') === undefined
 }
 
-export const getFigmaExport = async ({ accessToken, fileKey, ids = [] }: Props): Promise<FigmaExport[]> => {
+export const getFigmaExport = async ({accessToken, fileKey, ids = [], exportType}: Props): Promise<FigmaExport[]> => {
 
-  const api = new Api({
-    personalAccessToken: accessToken
-  })
+    const api = new Api({
+        personalAccessToken: accessToken
+    })
 
-  const { document: { children: pages } } = await api.getFile(fileKey, { ids })
+    const {document: {children: pages}} = await api.getFile(fileKey, {ids})
 
-  const groups = getGroups(pages)
+    const groups = getGroups(pages)
 
-  return await Promise.all(
-    groups.map(
-      async group => {
-        const frameIds = group.children.map(frame => frame.id)
+    return await Promise.all(
+        groups.map(
+            async group => {
+                const frameIds = group.children.map(frame => frame.id)
+                let ids = ""
 
-        const pdfResponse = await api.getImage(fileKey, {
-          ids: group.id,
-          format: 'png',
-          scale: 1
-        })
+                if (exportType.toLowerCase() === 'pdf') {
+                    ids = frameIds.join(',')
+                } else if (exportType.toLowerCase() === 'png') {
+                    ids = group.id
+                } else {
+                    throw new Error(`Unsupported export type: ${exportType}`)
+                }
 
-        if (pdfResponse.err) {
-          throw new Error(pdfResponse.err)
-        }
+                const pdfResponse = await api.getImage(fileKey, {
+                    ids: ids,
+                    format: exportType,
+                    scale: 1
+                })
 
-        const pages = Object.values(pdfResponse.images)
+                if (pdfResponse.err) {
+                    throw new Error(pdfResponse.err)
+                }
 
-        if (!pagesAreOk(pages)) {
-          throw new Error('Found empty pages!')
-        }
+                const pages = Object.values(pdfResponse.images)
 
-        const coverResponse = await api.getImage(fileKey, {
-          ids: frameIds[0],
-          format: 'jpg',
-          scale: 1
-        })
+                if (!pagesAreOk(pages)) {
+                    throw new Error('Found empty pages!')
+                }
 
-        if (coverResponse.err) {
-          throw new Error(coverResponse.err)
-        }
+                const coverResponse = await api.getImage(fileKey, {
+                    ids: frameIds[0],
+                    format: 'jpg',
+                    scale: 1
+                })
 
-        const [ cover ] = Object.values(coverResponse.images)
+                if (coverResponse.err) {
+                    throw new Error(coverResponse.err)
+                }
 
-        if (!cover) {
-          throw new Error('Cannot create cover!')
-        }
+                const [cover] = Object.values(coverResponse.images)
 
-        return {
-          id: group.id,
-          name: group.name,
-          pages,
-          cover
-        }
-      }
+                if (!cover) {
+                    throw new Error('Cannot create cover!')
+                }
+
+                return {
+                    id: group.id,
+                    name: group.name,
+                    pages,
+                    cover
+                }
+            }
+        )
     )
-  )
 }
--- src/runner.ts
+++ src/runner.ts
@@ -1,83 +1,87 @@
 import * as core from '@actions/core'
-import { mkdirSync, writeFileSync } from 'fs'
+import {mkdirSync, writeFileSync} from 'fs'
 import fetch from 'node-fetch'
-import path, { sep } from 'path'
+import path, {sep} from 'path'
 import PDFMerger from 'pdf-merger-js'
 
-import { getFigmaExport, FigmaExport } from './export'
+import {getFigmaExport, FigmaExport} from './export'
 
 type Options = {
-  accessToken: string
-  fileKey: string
-  ids: string[]
-  outDir: string
-
-  exportType: string
+    accessToken: string
+    fileKey: string
+    ids: string[]
+    outDir: string
+    exportType: 'jpg' | 'png' | 'svg' | 'pdf'
 }
 
 type Result = {
-  id: string
-  name: string
-  basename: string
-  filepath: string
-  cover: string
+    id: string
+    name: string
+    basename: string
+    filepath: string
+    cover: string
 }
 
-export async function run({ accessToken, fileKey, ids, outDir, exportType }: Options): Promise<Result[]> {
+export async function run({accessToken, fileKey, ids, outDir, exportType}: Options): Promise<Result[]> {
 
-  const pdfs = await getFigmaExport({
-    accessToken,
-    fileKey,
-    ids,
-  })
+    const diagrams = await getFigmaExport({
+        accessToken,
+        fileKey,
+        ids,
+        exportType,
+    })
 
-  console.log("Export type : " + exportType)
+    const result: Result[] = []
+    console.log("Start exporting diagrams in " + exportType);
 
-  const result: Result[] = []
-  console.log("Start export");
+    for (const diagram of diagrams) {
 
-  for (const pdf of pdfs) {
-    const pdfMerger = new PDFMerger();
+        const pages = await Promise.all(
+            diagram.pages.map(
+                async page => Buffer.from(await fetch(page).then(r => r.arrayBuffer()))
+            )
+        )
 
-    const pages = await Promise.all(
-      pdf.pages.map(
-        async page => Buffer.from(await fetch(page).then(r => r.arrayBuffer()))
-      )
-    )
+        const cover = Buffer.from(await fetch(diagram.cover).then(r => r.arrayBuffer()))
 
-    const cover = Buffer.from(await fetch(pdf.cover).then(r => r.arrayBuffer()))
+        const coverFilename = `${diagram.name}.jpg`
+        const pdfFilename = `${diagram.name}.pdf`
+        const pdfFilepath = path.resolve(outDir, pdfFilename)
+        const pdfBasename = path.basename(pdfFilename)
 
+        mkdirSync(path.dirname(pdfFilepath), {recursive: true})
 
-    const coverFilename = `${pdf.name}.jpg`
-    const pdfFilename = `${pdf.name}.pdf`
-    const pdfFilepath = path.resolve(outDir, pdfFilename)
-    const pdfBasename = path.basename(pdfFilename)
+        core.info(pdfBasename)
 
-    mkdirSync(path.dirname(pdfFilepath), { recursive: true })
+        // compare case-insensitive export type with PDF or PNG
+        if (exportType.toLowerCase() === "pdf") {
+            const pdfMerger = new PDFMerger();
+            for (const page of pages) {
+                await pdfMerger.add(page)
+            }
+            await pdfMerger.save(path.resolve(outDir, pdfFilename))
+        } else if (exportType.toLowerCase() === "png") {
+            let pngFileCount = 0
+            for (const page of pages) {
+                let pngFileName = `${diagram.name}_${pngFileCount}.png`
+                console.log(pngFileName);
+                writeFileSync(path.resolve(outDir, pngFileName), page)
+                pngFileCount = pngFileCount + 1
+            }
+        } else {
+            throw new Error(`Unsupported export type: ${exportType}`)
+        }
 
-    //core.info(pdfBasename)
+        writeFileSync(path.resolve(outDir, coverFilename), cover)
 
-    let pngFileCount = 0
-    for (const page of pages) {
-      let pngFileName = `${pdf.name}_${pngFileCount}.png`
-      console.log(pngFileName);
-      writeFileSync(path.resolve(outDir, pngFileName), page)
-      pngFileCount = pngFileCount + 1
-      // await pdfMerger.add(page)
+        result.push({
+            id: diagram.id,
+            name: path.basename(diagram.name),
+            basename: pdfBasename,
+            filepath: `.${sep}${path.join(path.basename(outDir), pdfFilename)}`,
+            cover: `.${sep}${path.join(path.basename(outDir), coverFilename)}`,
+        })
     }
 
-
-    // await pdfMerger.save(path.resolve(outDir, pdfFilename))
-    writeFileSync(path.resolve(outDir, coverFilename), cover)
-
-    result.push({
-      id: pdf.id,
-      name: path.basename(pdf.name),
-      basename: pdfBasename,
-      filepath: `.${sep}${path.join(path.basename(outDir), pdfFilename)}`,
-      cover: `.${sep}${path.join(path.basename(outDir), coverFilename)}`,
-    })
-  }
-
-  return result
+    return result
 }
