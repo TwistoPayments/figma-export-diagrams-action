f760b99d005e0424aad8aad48d56e070c5500b0c
paveldigana <pavel.digana@email.cz>
2023-01-25 14:22:03 +0100

    Added logging of output to index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ const fileKey = core.getInput('fileKey', {required: true})
 const exportType = core.getInput('exportType', {required: true}).toLowerCase()
 const ids = jsonParse<string[]>(core.getInput('ids', {required: false}) || '[]')
 
-;(async function () {
+;(async function() {
 
     if (ids === false) {
         core.setFailed('"ids" must be a stringified array of strings.')
@@ -38,11 +38,11 @@ const ids = jsonParse<string[]>(core.getInput('ids', {required: false}) || '[]')
     }
 
     core.startGroup('Export diagrams')
-    const pdfs = await run({accessToken, fileKey, ids, outDir, exportType})
+    const pdfs = await run({ accessToken, fileKey, ids, outDir, exportType })
     console.log("End group");
     core.endGroup()
 
-    mkdirSync(path.resolve(outDir), {recursive: true})
+    mkdirSync(path.resolve(outDir), { recursive: true })
 
     if (pdfs.length === 0) {
         core.warning('No Diagrams has been exported.')
